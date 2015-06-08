46c329564c21289f71ba60a7a4a6e9509b4f16a6
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-06-08 04:10:32 +0200

    Added a merge script
    basic ...

---
+++ merge.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2015 Tuan-Tu Tran
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import pygithub3
+
+import sys
+import logging
+import subprocess
+import os
+import re
+import pygithub3
+logging.basicConfig(level=logging.DEBUG)
+
+def shell(cmd, *accepted_codes):
+    p=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    out,err=p.communicate()
+    if p.returncode!=0 and p.returncode not in accepted_codes:
+        raise RuntimeError, ("shell error",cmd,p.returncode, accepted_codes)
+    return out
+
+def githubConfig(key):
+    item = shell("git config github."+key, 1)
+    if not item:
+        logging.error("github."+key+" is not configured")
+        sys.exit(1)
+    return item.strip()
+logging.debug(sys.argv)
+
+login = githubConfig("login")
+password = githubConfig("password")
+repo = githubConfig("repo")
+
+prNumber = int(sys.argv[1])
+msgFile = "/tmp/githubprmsg"
+shell("gvim %s"%msgFile)
+lines=[]
+with open(msgFile) as fh:
+    for l in fh:
+        if not l.startswith("#"):
+            lines.append(l)
+msg="".join(lines).strip()
+if msg!="":
+    g=pygithub3.Github(login=login, password=password, user=login, repo=repo)
+    g.pull_requests.merge(prNumber, msg)
+else:
+    logging.warn("aborting because of empty msg")
+    sys.exit(1)
