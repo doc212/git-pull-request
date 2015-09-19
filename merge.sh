d556defdf9183c4d9088b90e3d634c14e9b4cccb
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-09-19 02:15:46 +0200

    extracted variables

--- merge.sh
+++ merge.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
 
+###################################################################
+#
+# You can change some values here if you have "non-standard" needs
+#
+###################################################################
+
+#the command to invoke merge.py
+MERGE=merge-pull-request
+
+#the remote name
+ORIGIN=origin
+
+#the master branch name
+MASTER=master
+
+###################################################################
+
 PROG=`basename "$0"`
 
 if [ "$#" -ne 1 ]
@@ -10,11 +27,11 @@ error: missing PULL_REQUEST_NUMBER argument
 
 usage: $PROG PULL_REQUEST_NUMBER
 
-* merge the given pull request with an empty message by invoking "git-pull-request #pr-number -e"
-* then update local master with origin master (ff only) by invoking git fetch origin master:master
-* checks out master
-* then for each merge branch :
-    * delete the remote branch with the same name on origin
+* merge the given pull request with an empty message by invoking "$MERGE #pr-number -e"
+* then update local branch $MASTER with branch with the same name on the remote ($ORIGIN) (ff only) by invoking git fetch $ORIGIN $MASTER:$MASTER
+* checks out $MASTER
+* then for each merged branch :
+    * delete the remote branch with the same name on $ORIGIN
     * delete the local branch
 EOF
     exit 1
@@ -25,13 +42,13 @@ set -e
 echo $ merge-pull-request $1
 merge-pull-request $1 -e
 
-echo $ git fetch origin master:master
-git fetch origin master:master
-echo $ git checkout master
-git checkout master
+echo $ git fetch $ORIGIN $MASTER:$MASTER
+git fetch $ORIGIN $MASTER:$MASTER
+echo $ git checkout $MASTER
+git checkout $MASTER
 
 echo $ delete remote merged branches
-git branch --merged|grep master -v|xargs -i git push origin :{}
+git branch --merged|grep $MASTER -v|xargs -i git push $ORIGIN :{}
 
 echo $ delete local merged branches
-git branch --merged|grep master -v|xargs git branch -d
+git branch --merged|grep $MASTER -v|xargs git branch -d
